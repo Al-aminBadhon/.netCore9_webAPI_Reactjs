e70906bfc80345f3da3f6984ab58efb4bd55860a
B M Al-amin Badhon <alamin.badhonn@gmail.com>
2025-01-15 23:30:39 -0330

    edit

--- MyApp/MyApp.Api/obj/Debug/net9.0/MyApp.Api.AssemblyInfo.cs
+++ MyApp/MyApp.Api/obj/Debug/net9.0/MyApp.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e48f7ce70aced10f08edebd25e522c83cc51f55f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+610a4a69e476c43a51fb3aadbd5873739fb6eb16")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
